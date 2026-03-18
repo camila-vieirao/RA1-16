5568a98621611738549b53dda348d0d323eac2b5
camila-vieirao <camila.zero70505@gmail.com>
2026-03-17 23:42:46 -0300

    feat: general documentation and project structure

---
+++ legacy/README.md
@@ -0,0 +1,3 @@
+# Pasta legacy
+
+> **podemos excluir depois**. vou deixar essa pasta aqui por enquanto para caso a gente decida ir trabalhando em arquivos diferentes e juntar o código em um único arquivo só no final (como tá nos requisitos do RA)
---
+++ src/ra1.py
@@ -0,0 +1,19 @@
+# INTEGRANTES:
+# anabelly... | \
+# andressa... | \
+# Camila Vieira de Oliveira | camila-vieirao
+# guilherme... | \
+#
+# NOME DO GRUPO: RA1-16
+
+def parseExpressao():
+    pass
+
+def executarExpressao():
+    pass
+
+def gerarAssembly():
+    pass
+
+def exibirResultados():
+    pass
