4d4dd1d8ef184fb7000e2fc043f4db6649aeda6e
camila-vieirao <camila.zero70505@gmail.com>
2026-03-19 18:29:47 -0300

    feat: funcao para leitura de arquivo

--- src/ra1.py
+++ src/ra1.py
@@ -6,6 +6,21 @@
 #
 # NOME DO GRUPO: RA1-16
 
+def lerArquivo(file_path: str) -> list:
+    """
+    Lê o arquivo de entrada e armazena as expressões para processamento.
+
+    Args: 
+        file_path (str): Caminho do arquivo de entrada.
+    Returns:
+        list: vetor de linhas
+    """
+    linhas = []
+    for line in open(file_path, 'r'):
+        print(line)
+        linhas.append(line)
+    return linhas
+
 def parseExpressao():
     pass
 
@@ -17,3 +32,13 @@ def gerarAssembly():
 
 def exibirResultados():
     pass
+
+if __name__ == "__main__":
+    import sys
+    arquivo = sys.argv[1]
+    lerArquivo(arquivo)
+
+#     parseExpressao()
+#     executarExpressao()
+#     exibirResultados()
+#
