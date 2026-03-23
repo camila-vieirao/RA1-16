1eba1c99618667cc3dffb078d2a628dec2daac6c
Guilhermeffda <guiffa05@hotmail.com>
2026-03-23 20:24:22 -0300

    modificações para mudar o local do lexico

--- src/ra1.py
+++ src/ra1.py
@@ -6,7 +6,15 @@
 #
 # NOME DO GRUPO: RA1-16
 
-from lexico_afd import ErroLexico, parseExpressao as parse_expressao_afd
+from pathlib import Path
+import sys
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.insert(0, str(PROJECT_ROOT))
+
+from legacy.lexico_afd import ErroLexico, parseExpressao as parse_expressao_afd
 
 def lerArquivo(file_path: str) -> list:
     """
